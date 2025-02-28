fbf270be4df9147896da3acb044ece29f2cda169
Güllüzar Durmuş <guldurmss@gmail.com>
2025-02-28 14:00:28 +0300

    odev1 tamamlandı

---
+++ odev1/odev1/Classes/Araba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev1.Classes
+{
+    public class Araba
+    {
+        public string vites;
+        public string duz;
+        public string otomatik;
+        public string marka;
+        
+        public void vitesmarkayaz(string marka, string vites)
+        {
+            Console.WriteLine("Arabanın markası : " + marka + "\nVites turu : " + vites);
+        }
+    }
+}
---
+++ odev1/odev1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using odev1.Classes;
+
+Console.WriteLine("Hello, World!");
+
+Araba ar = new Araba();
+ar.marka = "BMW";
+ar.vites = "Otomatik";
+
+BMW b = new BMW();
+b.vitesmarkayaz("BMW", "Otomatik vitestir.");
+
+Audi a = new Audi();
+a.vitesmarkayaz("Audi", "Duz vitestir.");
+
+Porsche p = new Porsche();
+p.vitesmarkayaz("Porsche", "Otomatik vitestir.");
+
+Togg togg = new Togg();
+togg.vitesmarkayaz("Togg", "Otomatik vitestir.");
+
+Toyota t = new Toyota();
+t.vitesmarkayaz("Toyota", "Duz vitestir.");
+
+Mercedes m = new Mercedes();
+m.vitesmarkayaz("Mercedes", "Otomatik vitestir.");
