1230c6bb8384ec734fef40600698ddbc43cb43f3
Güllüzar Durmuş <guldurmss@gmail.com>
2025-03-14 17:54:37 +0300

    hello-world

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba, Dünya!");
+        }
+    }
+}
