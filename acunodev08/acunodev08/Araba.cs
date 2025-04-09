abdf9bc915290516971d488fb656c20c5213d8ce
Güllüzar Durmuş <guldurmss@gmail.com>
2025-04-09 13:36:20 +0300

    09nisanodev

---
+++ acunodev08/acunodev08/Araba.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace acunodev08
+{
+    public class Araba
+    {
+        public string Marka { get; set; }               
+        public string Model { get; set; }               
+        public double YakitTuketimi { get; set; }     
+        public double ToplamMesafe { get; set; }    
+
+        public double YakitHesapla()
+        {
+            return (ToplamMesafe / 100) * YakitTuketimi;
+        }
+    }
+}
---
+++ acunodev08/acunodev08/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acunodev08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Araba> Liste = new List<Araba>()
+            {
+                new Araba { Marka = "Toyota", Model = "Corolla", YakitTuketimi = 6.5, ToplamMesafe = 5000 },
+                new Araba { Marka = "BMW", Model = "X5", YakitTuketimi = 9.0, ToplamMesafe = 5000 },
+                new Araba { Marka = "Mercedes", Model = "C200", YakitTuketimi = 8.2, ToplamMesafe = 5000 }
+            };
+
+            foreach (var araba in Liste)
+            {
+                double toplamTuketim = araba.YakitHesapla();
+                Console.WriteLine($"Marka: {araba.Marka}, Model: {araba.Model}, 100 km'de Yakıt Tüketimi: {araba.YakitTuketimi} litre, Toplam Yakıt Tüketimi: {toplamTuketim} litre");
+            }
+        }
+    }
+}
