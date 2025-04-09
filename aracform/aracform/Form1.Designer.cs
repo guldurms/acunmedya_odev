f942c0f4c477ca9857e610c0e2ef6c851fd7f2fe
Güllüzar Durmuş <guldurmss@gmail.com>
2025-04-09 22:17:19 +0300

    formaracodevi

---
+++ aracform/aracform/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace aracform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(255, 224, 192);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(943, 128);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(53, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(190, 110);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(700, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(197, 110);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Script", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(308, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(339, 67);
+            label1.TabIndex = 0;
+            label1.Text = "Rose Otomotiv";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(255, 224, 192);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Font = new Font("Georgia", 12F, FontStyle.Italic, GraphicsUnit.Point, 162);
+            groupBox1.Location = new Point(12, 165);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(947, 375);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Araç bilgileri";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(326, 283);
+            button1.Name = "button1";
+            button1.Size = new Size(223, 50);
+            button1.TabIndex = 12;
+            button1.Text = "BİLGİ GETİR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(653, 177);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(140, 30);
+            textBox6.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(653, 126);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(146, 30);
+            textBox5.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(654, 64);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(145, 30);
+            textBox4.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(137, 183);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(149, 30);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(137, 126);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(149, 30);
+            textBox2.TabIndex = 7;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(408, 182);
+            label7.Name = "label7";
+            label7.Size = new Size(239, 24);
+            label7.TabIndex = 6;
+            label7.Text = "100 Km'de Yaktığı Yakıt :";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(496, 132);
+            label6.Name = "label6";
+            label6.Size = new Size(151, 24);
+            label6.TabIndex = 5;
+            label6.Text = "Pencere Sayısı :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(523, 70);
+            label5.Name = "label5";
+            label5.Size = new Size(124, 24);
+            label5.TabIndex = 4;
+            label5.Text = "Kapı Sayısı :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(58, 189);
+            label4.Name = "label4";
+            label4.Size = new Size(69, 24);
+            label4.TabIndex = 3;
+            label4.Text = "Renk :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(47, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 24);
+            label3.TabIndex = 2;
+            label3.Text = "Model :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(47, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 24);
+            label2.TabIndex = 1;
+            label2.Text = "Marka :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(137, 64);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(149, 30);
+            textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 255);
+            ClientSize = new Size(970, 542);
+            Controls.Add(groupBox1);
+            Controls.Add(panel1);
+            Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private GroupBox groupBox1;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+    }
+}
